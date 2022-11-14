a9a8f2a7643a0a14f2843cd6746d8e2653ce5134
Xavier Brigode <xavier.brigode@gmail.com>
2022-11-14 16:28:12 +0100

    mobile modale ending

---
+++ assets/scripts/modal.js
@@ -0,0 +1,12 @@
+const openBtn = document.querySelector(".modal-open")
+const closeBtn = document.querySelector(".modal-close")
+const modal = document.querySelector(".modal")
+
+function openModal(){
+    modal.style.visibility = "visible"
+}
+
+function closeModal(){
+    modal.style.visibility = "hidden"
+}
+
